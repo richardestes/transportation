9cbb75cef75541fb6ec21bf52909eb4e3e994f1d
Richard Estes <richard.estes48@gmail.com>
2026-07-13 01:10:41 -0700

    Started some shit

---
+++ scripts/main.gd
@@ -0,0 +1,10 @@
+extends Node
+
+@onready var button: Button = $HudLayer/HudRoot/Background/HSplitContainer/PanelContainer/VBoxContainer/Button
+@onready var label: Label = $HudLayer/HudRoot/Background/HSplitContainer/VBoxContainer/Label
+
+func _ready() -> void:
+	button.pressed.connect(_button_pressed)
+
+func _button_pressed() -> void:
+	label.text = str("Button Pressed")
